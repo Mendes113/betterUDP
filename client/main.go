3beeffa66db7edcf2f51a4ff476df41a98f0344a
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-06-11 20:54:59 -0300

    feat: :sparkles: Primeira versão betterUDP

--- client/main.go
+++ client/main.go
@@ -1,55 +1,86 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "strconv"
-    "time"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
 )
 
 const (
-    WINDOW_SIZE = 50
-    TOTAL_MESSAGES = 1000
+	TOTAL_MESSAGES = 100
+	MAX_RETRIES    = 100    
+	TIMEOUT        = 1 * time.Second
 )
 
 func main() {
-    arguments := os.Args
-    if len(arguments) == 1 {
-        fmt.Println("Please provide a host:port string")
-        return
-    }
-    CONNECT := arguments[1]
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a host:port string")
+		return
+	}
+	CONNECT := arguments[1]
 
-    s, err := net.ResolveUDPAddr("udp4", CONNECT)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	s, err := net.ResolveUDPAddr("udp4", CONNECT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    c, err := net.DialUDP("udp4", nil, s)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	c, err := net.DialUDP("udp4", nil, s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
-    defer c.Close()
+	defer c.Close()
 
-    for i := 0; i < TOTAL_MESSAGES; i++ {
-        // Construct message
-        message := "Message " + strconv.Itoa(i+1)
+	for i := 0; i < TOTAL_MESSAGES; i++ {
+		// Construct message
+		message := "Message " + strconv.Itoa(i+1)
 
-        // Send message
-        _, err = c.Write([]byte(message))
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
+		retries := 0
+		for {
+			// Send message
+			_, err = c.Write([]byte(message))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 
-        // Wait for a short duration to simulate real-time communication
-        time.Sleep(time.Millisecond * 100)
-    }
+			// Wait for ACK
+			ackBuf := make([]byte, 1024)
+			c.SetReadDeadline(time.Now().Add(TIMEOUT))
+			n, _, err := c.ReadFromUDP(ackBuf)
+			if err != nil {
+				fmt.Printf("No ACK received for message: %s, retrying...\n", message)
+				retries++
+				if retries >= MAX_RETRIES {
+					fmt.Printf("Maximum retries exceeded for message: %s, unable to send further messages.\n", message)
+					return
+				}
+				continue
+			}
 
-    fmt.Printf("Sent %d messages\n", TOTAL_MESSAGES)
+			ack := string(ackBuf[:n])
+			expectedAck := strconv.Itoa(i + 1)
+			if ack == expectedAck {
+				fmt.Printf("Received ACK: %s\n", ack)
+				break
+			} else {
+				fmt.Printf("Received incorrect ACK: %s, expecting: %s, retrying...\n", ack, expectedAck)
+				retries++
+				if retries >= MAX_RETRIES {
+					fmt.Printf("Maximum retries exceeded for message: %s, unable to send further messages.\n", message)
+					return
+				}
+			}
+		}
+
+		// Wait for a short duration to simulate real-time communication
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	fmt.Printf("Sent %d messages\n", TOTAL_MESSAGES)
 }
--- server/server.go
+++ server/server.go
@@ -1,83 +1,96 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "net"
-    "os"
-    "strconv"
-    "strings"
-    "time"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
 const (
-    WINDOW_SIZE = 50
+	WINDOW_SIZE = 10
 )
 
 var (
-    lastAckedSeqNum int
-    nextSeqNum      int
-    congestion      bool
+	nextSeqNum   int
+	baseSeqNum   int
+	congestion   bool
+	receiveQueue []string
+	mutex        sync.Mutex
 )
 
-func random(min, max int) int {
-    return rand.Intn(max-min) + min
-}
-
 func main() {
-    arguments := os.Args
-    if len(arguments) == 1 {
-        fmt.Println("Please provide a port number!")
-        return
-    }
-    PORT := ":" + arguments[1]
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+	PORT := ":" + arguments[1]
 
-    s, err := net.ResolveUDPAddr("udp4", PORT)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	s, err := net.ResolveUDPAddr("udp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    connection, err := net.ListenUDP("udp4", s)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	connection, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    defer connection.Close()
-    buffer := make([]byte, 1024)
-    rand.Seed(time.Now().Unix())
+	defer connection.Close()
+	buffer := make([]byte, 1024)
 
-    for {
-        n, addr, err := connection.ReadFromUDP(buffer)
-        fmt.Print("-> ", string(buffer[0:n-1]))
+	for {
+		n, addr, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-        if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
-            fmt.Println("Exiting UDP server!")
-            return
-        }
+		msg := strings.TrimSpace(string(buffer[:n]))
+		fmt.Printf("Received: %s\n", msg)
 
-        data := []byte(strconv.Itoa(random(1, 1001)))
-        fmt.Printf("data: %s\n", string(data))
+		if msg == "STOP" {
+			fmt.Println("Exiting UDP server!")
+			return
+		}
 
-        // Simulating congestion
-        if congestion {
-            time.Sleep(time.Millisecond * time.Duration(random(50, 200)))
-            congestion = false
-        }
+		mutex.Lock()
+		receiveQueue = append(receiveQueue, msg)
+		mutex.Unlock()
 
-        // Send data if within window size
-        if nextSeqNum-lastAckedSeqNum < WINDOW_SIZE {
-            seqData := append([]byte(strconv.Itoa(nextSeqNum)), data...)
-            _, err = connection.WriteToUDP(seqData, addr)
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-            nextSeqNum++
-        } else {
-            congestion = true
-            fmt.Println("Congestion detected! Waiting...")
-        }
-    }
+		// Simulating congestion
+		if congestion {
+			time.Sleep(time.Millisecond * 100)
+			congestion = false
+		}
+
+		// Process message and send ACK
+		if nextSeqNum < baseSeqNum+WINDOW_SIZE && nextSeqNum < len(receiveQueue) {
+			expectedSeqNum := strconv.Itoa(nextSeqNum + 1)
+			ack := []byte(expectedSeqNum)
+			_, err = connection.WriteToUDP(ack, addr)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			nextSeqNum++
+		} else {
+			congestion = true
+			fmt.Println("Congestion detected! Waiting...")
+		}
+
+		// Update base sequence number
+		mutex.Lock()
+		if len(receiveQueue) > 0 && nextSeqNum >= baseSeqNum+len(receiveQueue) {
+			baseSeqNum++
+			receiveQueue = receiveQueue[1:]
+		}
+		mutex.Unlock()
+	}
 }
